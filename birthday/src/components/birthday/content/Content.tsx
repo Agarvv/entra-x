ac5c19701c5858e9a87fa3cfe997ff91617dd8dc
Agarvv <162643502+Agarvv@users.noreply.github.com>
2025-09-23 23:38:12 +0000

    me encanta

--- birthday/src/components/birthday/content/Content.tsx
+++ birthday/src/components/birthday/content/Content.tsx
@@ -1,3 +1,4 @@
+import { redirect } from "react-router-dom";
 import styles from "./Content.module.css"
 
 const Content = () => {
@@ -6,13 +7,30 @@ const Content = () => {
          <div className={styles.congratsCtn}> 
 
             <div className={styles.congrats}>
-              <h1>¡Feliz Cumple, Carla!</h1>
+              <h1>¡Feliz Cumple, <strong>Carla!</strong></h1>
             </div>
 
             <div className={styles.message}>
-               <p>df,mfflkmlfkmfklfkflmgkflmdg,dfgkfkdgdmgkdgldfkgd</p>
-               <p>f,f,dk,dsmflkfml,mlfd,mld,md,mvfd,mgvfd,</p>
-               <p>fd,fmfkmsfklsdfkdsmlfkdsmlfkmldfkmldskfmlfkmlds</p>
+               <p>Hoy, Domingo 28 de Septiembre de 2025, Carla, querida Prima, Cumples la edad de 16 años.
+                  Que otra mejor manera de que tu Primo Lucas te desee Muchos mas años y un Feliz, pero Muy Feliz 
+                  Cumpleaños, que esta?
+
+                  Mucha salud, felicidad, prosperidad, amor, paz y suerte en todo lo que te propongas hacer.
+                  
+                  y que asi sea tanto contigo, como con mis otros primos, Blanca, Alicia, Alex,
+                  David y Aitor. sin olvidarme tambien de los primos que tengo de parte de mi madre. tambien mis tios y tias, y mis abuelos y abuelas.
+
+                  Te deseo un dia Fructifero junto a tus Primos y que disfrutes este dia como ningun otro, que la felicidad, paz y amor nunca falte en ti.
+
+                  
+
+               </p>
+
+               <p>
+                 Espero que te haya gustado mi sorpresa, Atentamente Con Mucho Cariño... <strong style={{
+         color: "red"
+      }}>Lucas</strong>
+               </p>
             </div>
 
          </div>
