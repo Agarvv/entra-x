797f70e931e4ef786c7d6d2de053bcd87f9da1f5
Agarvv <casluagarv@gmail.com>
2025-09-24 02:22:53 +0200

    issues

--- birthday/src/components/birthday/content/Content.tsx
+++ birthday/src/components/birthday/content/Content.tsx
@@ -11,23 +11,27 @@ const Content = () => {
             </div>
 
             <div className={styles.message}>
-               <p>Hoy, Domingo 28 de Septiembre de 2025, Carla, querida Prima, Cumples la edad de 16 años.
-                  Que otra mejor manera de que tu Primo Lucas te desee Muchos mas años y un Feliz, pero Muy Feliz 
-                  Cumpleaños, que esta?
+                <p>
+                    
+                    
+                    Hoy, domingo 28 de septiembre de 2025, querida Carla...
 
-                  Mucha salud, felicidad, prosperidad, amor, paz y suerte en todo lo que te propongas hacer.
-                  
-                  y que asi sea tanto contigo, como con mis otros primos, Blanca, Alicia, Alex,
-                  David y Aitor. sin olvidarme tambien de los primos que tengo de parte de mi madre. tambien mis tios y tias, y mis abuelos y abuelas.
+Felicidades por tus 16 años. No hay mejor manera de que tu primo Lucas te desee muchísimos años más de alegría y un cumpleaños muy, pero muy feliz...
 
-                  Te deseo un dia Fructifero junto a tus Primos y que disfrutes este dia como ningun otro, que la felicidad, paz y amor nunca falte en ti.
+Te deseo mucha salud, felicidad, amor, paz, prosperidad y suerte en todo lo que te propongas. Que todo lo bueno que desees se haga realidad, tanto para ti como para tu hermana Blanca, nuestros primos Alicia, Alex, David y Aitor, y sin olvidarme también de mis otros primos de parte de mi madre, así como para nuestros tíos, tías y abuelos.
 
-                  
+Espero que tengas un día... inolvidable junto a tus primos y que lo disfrutes como ningún otro. Que la felicidad, la paz y el amor nunca falten en tu vida.
 
-               </p>
+
+                </p>
+               
+               
+               
+               
+               
 
                <p>
-                 Espero que te haya gustado mi sorpresa, Atentamente Con Mucho Cariño... <strong style={{
+                 Espero que te haya gustado mi sorpresa. Con Mucho Cariño... <strong style={{
          color: "red"
       }}>Lucas</strong>
                </p>
