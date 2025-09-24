7efcf6d3fba87db69cdbec7c33725e16d56b92ea
Agarvv <casluagarv@gmail.com>
2025-09-24 02:43:01 +0200

    issues

--- birthday/src/components/home/show-button/show-button.tsx
+++ birthday/src/components/home/show-button/show-button.tsx
@@ -1,10 +1,11 @@
 import { useNavigate } from 'react-router-dom'
 import { useEffect, useRef } from 'react'
+import styles from "./show-button.module.css"
 
 const ShowButton = () => {
   const navigate = useNavigate()
 
-  // Creamos los audios solo una vez
+ 
   const audioOne = useRef(new Audio("/partyblower.mp3"))
   const audioTwo = useRef(new Audio("/happy-birthday-334876.mp3"))
 
@@ -15,18 +16,18 @@ const ShowButton = () => {
 
   const surprise = async () => {
     try {
-      // Reproducir ambos audios al mismo tiempo
+      
       await Promise.all([audioOne.current.play(), audioTwo.current.play()])
 
-      // Luego navegar
+      
       navigate("/birthday")
     } catch (err) {
       console.log("Error al reproducir audio:", err)
-      navigate("/birthday") // aunque falle el audio, seguimos con la ruta
+      navigate("/birthday") 
     }
   }
 
-  return <button onClick={surprise}>Hola</button>
+  return <button className={styles.button} onClick={surprise}>Hola</button>
 }
 
 export default ShowButton
